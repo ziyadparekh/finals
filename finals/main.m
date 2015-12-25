95168f20b51ed2a75efbe8d3af520a0f4997c738
ziyadparekh <ziyad@1stdibs.com>
2015-12-24 18:15:16 -0800

    Initial Commit

---
+++ finals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  finals
+//
+//  Created by Ziyad Parekh on 12/24/15.
+//  Copyright (c) 2015 Ziyad Parekh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
